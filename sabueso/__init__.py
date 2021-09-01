8a311a284a7b4055b4e45135575ca55e78c5c553
Diego Prada <prada.gracia@gmail.com>
2021-08-31 20:12:33 -0500

    Adding files from MOLSSI Cookie-cutter

--- sabueso/__init__.py
+++ sabueso/__init__.py
@@ -1,3 +1,17 @@
+"""
+Sabueso
+This must be a short description of the project
+"""
+
+# Handle versioneer
+from ._version import get_versions
+versions = get_versions()
+__version__ = versions['version']
+__git_revision__ = versions['full-revisionid']
+del get_versions, versions
+
+# Add imports here
+
 #from .protein import Protein
 from .pdb import PDB
 #from .query import target as query_target
--- setup.py
+++ setup.py
@@ -1,24 +1,47 @@
+"""
+ProjectName
+A short description of the project.
+"""
+import sys
 from setuptools import setup, find_packages
-from numpy.distutils.core import setup
-from numpy.distutils.extension import Extension
-#import distutils.extension
+import versioneer
 
-extensions_list=[]
+short_description = __doc__.split("\n")
+
+# from https://github.com/pytest-dev/pytest-runner#conditional-requirement
+needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
+pytest_runner = ['pytest-runner'] if needs_pytest else []
+
+try:
+    with open("README.md", "r") as handle:
+        long_description = handle.read()
+except:
+    long_description = "\n".join(short_description[2:])
+
 
 setup(
-    name='sabueso',
-    version='0.0.1',
+    name='wabueso',
     author='UIBCDF Lab',
     author_email='uibcdf@gmail.com',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     package_dir={'sabueso': 'sabueso'},
     packages=find_packages(),
-    ext_modules=extensions_list,
-    package_data={'sabueso': []},
+    include_package_data=True,
+    package_data={'sabueso': ['data']},
     scripts=[],
-    url='http://uibcdf.org',
+    setup_requires=[] + pytest_runner,
+    platforms=['Linux',
+                'Mac OS-X',
+                'Unix',
+                'Windows',
+    ],
+    python_requires=">=3.7",
+    url='http://uibcdf.org/sabueso',
     download_url ='https://github.com/uibcdf/Sabueso',
     license='MIT',
-    description="---",
-    long_description="---",
+    description=short_description[0],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 )
 
