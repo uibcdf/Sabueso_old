69e13412084eb15f622ddd9d04337835ca9c2b78
Diego Prada <prada.gracia@gmail.com>
2022-01-31 19:01:42 -0600

    Adding method to convert string:uniprot_id to string:chembl_id in tools

--- sabueso/tools/database_ChEMBL/is_accessible.py
+++ sabueso/tools/database_ChEMBL/is_accessible.py
@@ -3,14 +3,14 @@ from sabueso._private_tools.exceptions import DatabaseNotAccessible
 
 def is_accessible():
 
-    r = requests.get('https://www.uniprot.org/uniprot/', stream=True, timeout=5)
+    r = requests.get('https://www.ebi.ac.uk/chembl/api/data', stream=True, timeout=5)
     return r.status_code == requests.codes.ok
 
 def _checking_database(check=True):
 
     if check:
         if not is_accessible():
-            raise DatabaseNotAccessible('UniProtKB')
+            raise DatabaseNotAccessible('ChEMBL')
 
     pass
 
--- sabueso/tools/database_UniProtKB/__init__.py
+++ sabueso/tools/database_UniProtKB/__init__.py
@@ -1,3 +1,3 @@
-from .is_accessible import is_accessible
+from .is_accessible import is_accessible, _checking_database
 from .query import query
 from .download import download
